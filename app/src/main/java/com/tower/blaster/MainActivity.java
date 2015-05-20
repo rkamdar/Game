b7b2687b78c5411cd243c16064b26027e50b26e8
rdevelopers <rdevelopers92@gmail.com>
2015-05-20 23:53:23 +0530

    For Scoreboard issue Change Scoreboard width to wrap_content Add it to outer_layout to make it free from the each mod layout

--- app/src/main/java/com/tower/blaster/MainActivity.java
+++ app/src/main/java/com/tower/blaster/MainActivity.java
@@ -143,7 +143,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.outer_layout);
 
         context = this;
 
